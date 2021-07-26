2dc287a419ef65859a6c6803f83fb856dd5e242f
Philipp Beck <dertieran@gmail.com>
2021-07-26 18:18:08 +0200

    :recycle: Fire did show/hide events after webview layout

--- src/android/KeyboardPlugin.java
+++ src/android/KeyboardPlugin.java
@@ -4,23 +4,34 @@ import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
-import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import org.apache.cordova.*;
 import org.json.JSONArray;
-import org.json.JSONException;
 
 public class KeyboardPlugin extends CordovaPlugin {
-    private View rootView;
-    private boolean keyboardWasOpen = false;
     private OnGlobalLayoutListener listener;
+    private boolean keyboardWasVisible = false;
+    private double KEYBOARD_MIN_HEIGHT_RATIO = 0.15;
 
     private InputMethodManager getInputManager() {
         return (InputMethodManager) cordova.getActivity().getSystemService(
             Context.INPUT_METHOD_SERVICE);
+    }
+
+    private View getContentView() {
+        return cordova.getActivity().findViewById(android.R.id.content);
+    }
+
+    private ViewTreeObserver getRootViewTreeObserver() {
+        return getContentView().getRootView().getViewTreeObserver();
+    }
+    private ViewTreeObserver getWebViewTreeObserver() {
+        return webView.getView().getViewTreeObserver();
     }
 
     private void fireWindowEvent(String event) {
@@ -29,10 +40,19 @@ public class KeyboardPlugin extends CordovaPlugin {
             webView.getEngine().evaluateJavascript(js, null);
         });
     }
+    private void fireWindowEventAfterWebViewLayout(String event) {
+        getWebViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                fireWindowEvent(event);
+                getWebViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
 
-    private int getScreenHeight() {
+    private int getScreenHeight(View root) {
         if (Build.VERSION.SDK_INT < 21) {
-            return rootView.getRootView().getHeight();
+            return root.getRootView().getHeight();
         }
 
         Point size = new Point();
@@ -41,42 +61,62 @@ public class KeyboardPlugin extends CordovaPlugin {
         return size.y;
     }
 
+    // NOTE: Determine if keyboard is visible
+    // (Implementation adapted from https://github.com/yshrsmz/KeyboardVisibilityEvent)
+    private boolean isKeyboardVisible() {
+        Rect r = new Rect();
+        View content = getContentView();
+        View root = content.getRootView();
+        root.getWindowVisibleDisplayFrame(r);
+
+        int[] location = new int[2];
+        content.getLocationOnScreen(location);
+
+        int screenHeight = getScreenHeight(root);
+        int heightDiff = screenHeight - r.height() - location[1];
+
+        return heightDiff > screenHeight * KEYBOARD_MIN_HEIGHT_RATIO;
+    }
+
+    private boolean isSoftInputAdjustNothing() {
+        int softInputMode = cordova.getActivity().getWindow().getAttributes().softInputMode;
+        int softInputAdjust = softInputMode & WindowManager.LayoutParams.SOFT_INPUT_MASK_ADJUST;
+        return (softInputAdjust & WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+            == WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING;
+    }
+
     @Override
     protected void pluginInitialize() {
         super.pluginInitialize();
 
-        DisplayMetrics dm = new DisplayMetrics();
-        cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        final float density = dm.density;
+        // The window will not be resized in case of SOFT_INPUT_ADJUST_NOTHING
+        if (isSoftInputAdjustNothing()) {
+            return;
+        }
 
-        // TODO: Maybe use setOnApplyWindowInsetsListener to implement the will show/hide events
-        rootView = cordova.getActivity().findViewById(android.R.id.content).getRootView();
         listener = () -> {
-            Rect r = new Rect();
-            rootView.getWindowVisibleDisplayFrame(r);
-            int heightDiff = getScreenHeight() - r.bottom;
-
-            // If the diff is more than 100 pixels, its probably a keyboard...
-            boolean keyboardIsOpen = (heightDiff / density) > 100;
-            if (keyboardWasOpen != keyboardIsOpen) {
-                if (keyboardIsOpen) {
+            boolean keyboardIsVisible = isKeyboardVisible();
+            if (keyboardWasVisible != keyboardIsVisible) {
+                if (keyboardIsVisible) {
                     fireWindowEvent("keyboardWillShow");
-                    fireWindowEvent("keyboardDidShow");
+                    fireWindowEventAfterWebViewLayout("keyboardDidShow");
                 } else {
                     fireWindowEvent("keyboardWillHide");
-                    fireWindowEvent("keyboardDidHide");
+                    fireWindowEventAfterWebViewLayout("keyboardDidHide");
                 }
             }
-            keyboardWasOpen = keyboardIsOpen;
+            keyboardWasVisible = keyboardIsVisible;
         };
 
-        rootView.getViewTreeObserver().addOnGlobalLayoutListener(listener);
+        getRootViewTreeObserver().addOnGlobalLayoutListener(listener);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        rootView.getViewTreeObserver().removeOnGlobalLayoutListener(listener);
+        if (listener != null) {
+            getRootViewTreeObserver().removeOnGlobalLayoutListener(listener);
+        }
     }
 
     @Override
